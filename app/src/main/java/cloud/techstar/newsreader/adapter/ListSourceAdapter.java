7e8d967252b4eff45c046a2d8d7df43d87ba4f8e
Doljinsuren Enkhbayar <doljko927@gmail.com>
2018-03-24 23:06:24 +0800

    List source adapter update

--- app/src/main/java/cloud/techstar/newsreader/adapter/ListSourceAdapter.java
+++ app/src/main/java/cloud/techstar/newsreader/adapter/ListSourceAdapter.java
@@ -7,31 +7,41 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import cloud.techstar.newsreader.common.Common;
+import cloud.techstar.newsreader.interfaces.IconBetterIdeaService;
 import cloud.techstar.newsreader.interfaces.ItemClickListener;
+import cloud.techstar.newsreader.models.IconBetterIdea;
 import cloud.techstar.newsreader.models.WebSite;
 import cloud.techstar.newsreader.R;
 import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Doljko on 3/22/2018.
  */
 
-class ListSourceViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+class ListSourceViewHolder extends RecyclerView.ViewHolder
+        implements View.OnClickListener
 {
     ItemClickListener itemClickListener;
 
     TextView source_title;
     CircleImageView source_image;
 
-     public ListSourceViewHolder(View itemView) {
-         super(itemView);
-
-         source_image=(CircleImageView) itemView.findViewById(R.id.source_image);
-         source_title=(TextView) itemView.findViewById(R.id.source_name);
-     }
-
-    public ListSourceViewHolder(View itemView, ItemClickListener itemClickListener) {
+    public ListSourceViewHolder(View itemView) {
         super(itemView);
+
+        source_image = (CircleImageView) itemView.findViewById(R.id.source_image);
+        source_title = (TextView)itemView.findViewById(R.id.source_name);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
         this.itemClickListener = itemClickListener;
     }
 
@@ -45,9 +55,13 @@ public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder
     private Context context;
     private WebSite webSite;
 
+    private IconBetterIdeaService mService;
+
     public ListSourceAdapter(Context baseContext, WebSite webSite) {
         this.context = baseContext;
         this.webSite = webSite;
+
+        mService = Common.getIconService();
     }
 
     @Override
@@ -58,8 +72,40 @@ public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder
     }
 
     @Override
-    public void onBindViewHolder(ListSourceViewHolder holder, int position) {
+    public void onBindViewHolder(final ListSourceViewHolder viewHolder, int position) {
 
+        StringBuilder iconBetterAPI = new StringBuilder("https://besticon-demo.herokuapp.com/icon?url=");
+        iconBetterAPI.append(webSite.getSources().get(position).getUrl());
+
+        mService.getIconUrl(iconBetterAPI.toString())
+                .enqueue(new Callback<IconBetterIdea>() {
+
+                    @Override
+                    public void onResponse(Call<IconBetterIdea> call, Response<IconBetterIdea> response) {
+
+                        if(response.body().getIcons().size()>0)
+                        {
+                            Picasso.with(context)
+                                    .load(response.body().getIcons().get(0).getUrl())
+                                    .into(viewHolder.source_image);
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<IconBetterIdea> call, Throwable t) {
+
+                    }
+                });
+
+        viewHolder.source_title.setText(webSite.getSources().get(position).getName());
+        viewHolder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                
+            }
+        });
     }
 
     @Override
