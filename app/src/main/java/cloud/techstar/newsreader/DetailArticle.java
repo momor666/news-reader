ad7e4bbd9590d0f9cb80381322884a2320ee7050
Doljinsuren Enkhbayar <doljko927@gmail.com>
2018-04-09 13:07:34 +0800

    Create article detail

---
+++ app/src/main/java/cloud/techstar/newsreader/DetailArticle.java
@@ -0,0 +1,41 @@
+package cloud.techstar.newsreader;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import cloud.techstar.newsreader.models.WebSite;
+import dmax.dialog.SpotsDialog;
+
+public class DetailArticle extends AppCompatActivity {
+
+    WebView webView;
+    AlertDialog dialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_article);
+
+        dialog = new SpotsDialog(this);
+        dialog.show();
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            dialog.dismiss();
+        }
+    });
+        if(getIntent() != null)
+         {
+                if(!getIntent().getStringExtra("webURL").isEmpty())
+                 webView.loadUrl(getIntent().getStringExtra("webURL"));
+         }
+
+}
